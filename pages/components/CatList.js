187880702aae9e8f987fa1019691c97a5e5e8c7d
andrewc2020 <andrewc2020@gmail.com>
2020-09-15 08:36:50 +0100

    reset

---
+++ pages/components/CatList.js
@@ -0,0 +1,34 @@
+import useSWR from 'swr';
+import Link from 'next/link'
+
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
+
+export default function CatList(props) {
+    
+     
+   
+     const { data, error } = useSWR(
+       `/api/cats`,
+       fetcher
+     );
+   
+     
+   return (
+   
+        <div>
+       <h1>Cats</h1>
+   
+       <ul>
+         {data && data.cats && data.cats.map(cat => (
+           <li key={cat._id}>
+             <Link href="/cats/[id]" as={`/cats/${cat.id}`}>
+               <a>{cat.name}</a>
+             </Link>
+             
+           </li>
+         ))}
+       </ul>
+       </div>
+   )};
