0d94b00933c3b9b91dcc16a31ceb207e601f5553
andrewc2020 <andrewc2020@gmail.com>
2020-09-16 18:23:15 +0100

    added bootstrap

--- components/signon.js
+++ components/signon.js
@@ -12,26 +12,26 @@ export default function Signup(){
   
   
   return <form  onSubmit={handleSubmit} >
-  <div>
-    <label>First Name</label>
-    <input type="text" name="firstName" onChange={handleInputChange} value={inputs.firstName} required />
-    <label>Last Name</label>
-    <input type="text" name="lastName" onChange={handleInputChange} value={inputs.lastName} required />
+  <div className="form-group">
+    <label for="firstName" >First Name</label>
+    <input type="text" name="firstName" onChange={handleInputChange} value={inputs.firstName} required className="form-control"/>
+    <label for="lastName">Last Name</label>
+    <input type="text" name="lastName" onChange={handleInputChange} value={inputs.lastName} required className="form-control"/>
   </div>
   <div>
-    <label>Email Address</label>
-    <input type="email" name="email" onChange={handleInputChange} value={inputs.email} required />
+    <label for="email">Email Address</label>
+    <input type="email" name="email" onChange={handleInputChange} value={inputs.email} required className="form-control"/>
   </div>
   <div>
-    <label>Password</label>
-    <input type="password" name="password1" onChange={handleInputChange} value={inputs.password1} />
+    <label for="password1">Password</label>
+    <input type="password" name="password1" onChange={handleInputChange} value={inputs.password1} className="form-control" required/>
   </div>
   <div>
-    <label>Re-enter Password</label>
-    <input type="password" name="password2" onChange={handleInputChange} value={inputs.password2}/>
+    <label for="password2">Re-enter Password</label>
+    <input type="password" name="password2" onChange={handleInputChange} value={inputs.password2} className="form-control" required/>
   </div>
-  <button type="submit"  >Sign Up</button>
-<div><label>{Submitted?"Welcome Aboard!":""}</label></div>
+  <button type="submit" className="btn btn-primary" >Sign Up</button>
+<div><label className="alert alert-primary" role="alert">{Submitted?"Welcome Aboard!":""}</label></div>
 
 </form>
 }
